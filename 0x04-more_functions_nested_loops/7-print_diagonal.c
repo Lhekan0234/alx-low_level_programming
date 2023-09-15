a7e7fed3380b6ff91c30973a7399b47c77ffbcbf
Your Name <you@example.com>
2023-09-14 18:01:23 -0700

    C is fun

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - Draws a diagonal line using the \ character.
+ * @n: The number of \ characters to be printed.
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j <= i; j++)
+			{
+				if (j != i)
+					_putchar(' ');
+				else
+					_putchar('\\');
+			}
+			_putchar('\n');
+		}
+	}
+}
